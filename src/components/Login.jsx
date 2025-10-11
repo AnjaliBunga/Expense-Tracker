470e37a076d75bee6e0ef18c6070dc0081e0d469
AnjaliBunga <anjalibunga130704@gmail.com>
2025-10-12 01:54:36 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = ({ onLogin, onSwitchToSignup }) => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://expense-tracker-u4r9.onrender.com/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
