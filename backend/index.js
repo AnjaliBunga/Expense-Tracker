2d71353c9cafc62c6a7f842e18dd660eaf177018
AnjaliBunga <anjalibunga130704@gmail.com>
2025-10-21 17:58:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(cors({
+  origin: 'https://fascinating-cranachan-dce869.netlify.app'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
